8f0952a8a751d0006c7da0ff8afa21884fd4f3d4
Kalen Dubrick <kalen3285@gmail.com>
2013-03-23 15:41:24 -0500

    Inital upload

---
+++ ITS140-DubrickKalen-Lab02.js
@@ -0,0 +1,22 @@
+/* =============================
+File name: ITS140-DubrickKalen-Lab02.js
+Description: Program to output textbook information
+Usage: cscript ITS140-DubrickKalen-Lab02.js
+© Kalen Dubrick 2012
+============================= */
+
+//Declare variables
+var bookTitle = "Programming Logic and Design Comprehensive";
+var bookEdition = "6th";
+var authorFullName = "Joyce Farrell";
+var publisherName = "Cengage Technology";
+var bookIsbn = 9780538744768
+var bookPrice = "way too much";
+var realBookPrice = 121.29;
+
+//Display information
+WScript.stdOut.WriteLine("Our book's title is " + bookTitle + ".  It is the " + bookEdition + " edition.  The author's name is " + authorFullName + ".  It is published by " + publisherName + ".  The book's ISBN is " + bookIsbn + ".  It costs " + bookPrice + ", or $" + realBookPrice + ".");
+
+/* ============================ 
+   |   © Kalen Dubrick 2012   |     
+   ============================ */
